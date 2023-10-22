98b28861fe65a8959065350b6a6973a8df6501a9
muhammadfikri <50939234+ClaysHere@users.noreply.github.com>
2023-10-23 01:46:28 +0700

    Update movie-ticket.kt

--- Unit2/Ptahway1/movie-ticket.kt
+++ Unit2/Ptahway1/movie-ticket.kt
@@ -1,16 +1,20 @@
 fun main() {
-    val morningNotification = 51
-    val eveningNotification = 135
+    val child = 5
+    val adult = 28
+    val senior = 87
     
-    printNotificationSummary(morningNotification)
-    printNotificationSummary(eveningNotification)
+    val isMonday = true
+    
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
 }
-
-
-fun printNotificationSummary(numberOfMessages: Int) {
-    if (numberOfMessages < 100) {
-        println("You have ${numberOfMessages} notifications.")
-    } else {
-        println("Your phone is blowing up! You have 99+ notifications.")
+ 
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    return when(age) {
+        in 0..12 -> 15
+        in 13..60 -> if (isMonday) 25 else 30
+        in 61..100 -> 20
+        else -> -1
     }
 }
