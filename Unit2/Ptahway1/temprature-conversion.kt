cfdd3935ac20967f59c56dcc09cba9df174a0597
muhammadfikri <50939234+ClaysHere@users.noreply.github.com>
2023-10-23 01:47:38 +0700

    Update and rename tempratureConversion.kt to temprature-conversion.kt

--- Unit2/Ptahway1/temprature-conversion.kt
+++ Unit2/Ptahway1/temprature-conversion.kt
@@ -1,3 +1,5 @@
+// My Solution
+
 fun main() {
     val celsiusToFahrenheit: (Double) -> Double = { celsius ->
     celsius * (9/5) + 32
@@ -16,6 +18,26 @@ fun main() {
     printFinalTemperature(10.0, "Fahrenheit", "Kelvin", fahrenheitToKelvin )
 }
 
+
+
+fun printFinalTemperature(
+    initialMeasurement: Double, 
+    initialUnit: String, 
+    finalUnit: String, 
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
+}
+
+
+// Solution
+
+fun main() {    
+        printFinalTemperature(27.0, "Celsius", "Fahrenheit") { 9.0 / 5.0 * it + 32 }
+        printFinalTemperature(350.0, "Kelvin", "Celsius") { it - 273.15 }
+        printFinalTemperature(10.0, "Fahrenheit", "Kelvin") { 5.0 / 9.0 * (it - 32) + 273.15 }
+}
 
 
 fun printFinalTemperature(
